eb045741c29ce9c3604549191eaad4deb3daeeae
kentsyx <kentsyx@gmail.com>
2020-12-02 04:21:43 -0500

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yunxiao/clones/tiny-vSLAM
-# Build directory: /home/yunxiao/clones/tiny-vSLAM/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
-subdirs("app")
--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yunxiao/clones/tiny-vSLAM/test
-# Build directory: /home/yunxiao/clones/tiny-vSLAM/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_triangulation "test_triangulation")
-set_tests_properties(test_triangulation PROPERTIES  _BACKTRACE_TRIPLES "/home/yunxiao/clones/tiny-vSLAM/test/CMakeLists.txt;6;ADD_TEST;/home/yunxiao/clones/tiny-vSLAM/test/CMakeLists.txt;0;")
